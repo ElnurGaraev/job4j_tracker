24437af9b50be8526771ca21588b98f57f510829
User <egaraev88@mail.ru>
2022-11-21 16:42:39 +0300

    2. Перегрузка конструктора [#173890]

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -4,6 +4,18 @@ public class Item {
     private int id;
     private String name;
 
+    public Item() {
+    }
+
+    public  Item(String name) {
+        this.name = name;
+    }
+
+    public Item(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
     public int getId() {
         return id;
     }
@@ -18,5 +30,21 @@ public class Item {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public void print() {
+        System.out.println("ID: " + id);
+        System.out.println("Name: " + name);
+    }
+
+    public static void main(String[] args) {
+        Item first = new Item();
+        first.print();
+        System.out.println();
+        Item second = new Item("ABC");
+        second.print();
+        System.out.println();
+        Item third = new Item(111, "ABCD");
+        third.print();
     }
 }
