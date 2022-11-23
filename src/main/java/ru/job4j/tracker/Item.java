f30f4983d75993d8b6e9dfc0e34e98786f6e5dd5
User <egaraev88@mail.ru>
2022-11-24 00:44:34 +0300

    9. toString [#310018]

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -40,4 +40,14 @@ public class Item {
     public LocalDateTime getCreated() {
         return created;
     }
+
+    @Override
+    public String toString() {
+        return "Item{"
+                + "id=" + id
+                + ", name='" + name + '\''
+                + ", created=" + created
+                + ", formatter=" + formatter
+                + '}';
+    }
 }
--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -1,8 +1,17 @@
 package ru.job4j.tracker;
 
+import javax.xml.crypto.Data;
+import java.time.LocalDateTime;
+
 public class StartUI {
     public static void main(String[] args) {
         Item date = new Item();
         System.out.println(date.getCreated());
+        System.out.println();
+        Item info = new Item();
+        info.getCreated();
+        info.setId(10);
+        info.setName("Laptop");
+        System.out.println(info);
     }
 }
