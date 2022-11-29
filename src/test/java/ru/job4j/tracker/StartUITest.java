8159108ae9c2054165d5a9c54f1a8c9d633f5a58
User <egaraev88@mail.ru>
2022-11-29 21:43:06 +0300

    1.1. Тесты на StartUI. [#223313 #365733]

--- src/test/java/ru/job4j/tracker/StartUITest.java
+++ src/test/java/ru/job4j/tracker/StartUITest.java
@@ -3,8 +3,6 @@ package ru.job4j.tracker;
 import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
 
-import static org.junit.Assert.*;
-
 public class StartUITest {
 
     @Test
