0652685439b21c765fcdd654df1a6bca7c736e3e
User <egaraev88@mail.ru>
2022-11-30 12:47:13 +0300

    2.1. Тест на ValidateInput [#311887 #365778]

--- src/test/java/ru/job4j/tracker/ValidateInputTest.java
+++ src/test/java/ru/job4j/tracker/ValidateInputTest.java
@@ -31,7 +31,7 @@ public class ValidateInputTest {
     @Test
     public void whenMultipleValidInput() {
         Output out = new StubOutput();
-        Input in = new StubInput(new String[]{"1", "1"}
+        Input in = new StubInput(new String[]{"1", "2"}
         );
         ValidateInput input = new ValidateInput(out, in);
         int selected = input.askInt("Enter menu");
