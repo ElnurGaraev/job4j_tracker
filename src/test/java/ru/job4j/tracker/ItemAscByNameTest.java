8cce280f877bc6950b966950e95a8729786bb94a
User <egaraev88@mail.ru>
2022-12-08 19:45:28 +0300

    0. Сортировка [#10096]

---
+++ src/test/java/ru/job4j/tracker/ItemAscByNameTest.java
@@ -0,0 +1,33 @@
+package ru.job4j.tracker;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.offset;
+
+import static org.junit.Assert.*;
+
+public class ItemAscByNameTest {
+
+    @Test
+    public void compare() {
+        List<Item> items = Arrays.asList(
+                new Item("Test2"),
+                new Item("Test1"),
+                new Item("Test3"),
+                new Item("Test5"),
+                new Item("Test4")
+                );
+        items.sort(new ItemAscByName());
+        List<Item> expected = Arrays.asList(
+                new Item("Test1"),
+                new Item("Test2"),
+                new Item("Test3"),
+                new Item("Test4"),
+                new Item("Test5")
+                );
+        assertThat(items).isEqualTo(expected);
+    }
+}
